e1d05809f5ba6dbd703a3772ed4579041c9f5857
Morten Niklas Sølling <eaamosol@students.eaaa.dk>
2016-10-11 12:07:45 +0200

    V0.1.1
    PlayerMoves implementeret.

--- Labyrint2016ElevCopy/src/game2016/Main2.java
+++ Labyrint2016ElevCopy/src/game2016/Main2.java
@@ -12,65 +12,52 @@ import java.util.Scanner;
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.geometry.Insets;
-import javafx.stage.Stage;
 import javafx.scene.Scene;
-import javafx.scene.control.*;
-import javafx.scene.image.*;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.GridPane;
-import javafx.scene.text.*;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
 
 public class Main2 extends Application {
 
-	public static final int size = 20; 
+	public static final int size = 20;
 	public static final int scene_height = size * 20 + 100;
 	public static final int scene_width = size * 20 + 200;
 
 	public static Image image_floor;
 	public static Image image_wall;
-	public static Image hero_right,hero_left,hero_up,hero_down;
+	public static Image hero_right, hero_left, hero_up, hero_down;
 
 	public static Player me;
 	public static List<Player> players = new ArrayList<Player>();
 
 	private Label[][] fields;
 	private TextArea scoreList;
-	
+
 	private Socket s;
 	private InputStream instream;
 	private OutputStream outstream;
 	private Scanner in;
 	private PrintWriter out;
-	
-	private  String[] board = {    // 20x20
-			"wwwwwwwwwwwwwwwwwwww",
-			"w        ww        w",
-			"w w  w  www w  w  ww",
-			"w w  w   ww w  w  ww",
-			"w  w               w",
-			"w w w w w w w  w  ww",
-			"w w     www w  w  ww",
-			"w w     w w w  w  ww",
-			"w   w w  w  w  w   w",
-			"w     w  w  w  w   w",
-			"w ww ww        w  ww",
-			"w  w w    w    w  ww",
-			"w        ww w  w  ww",
-			"w         w w  w  ww",
-			"w        w     w  ww",
-			"w  w              ww",
-			"w  w www  w w  ww ww",
-			"w w      ww w     ww",
-			"w   w   ww  w      w",
-			"wwwwwwwwwwwwwwwwwwww"
-	};
 
-	
+	private String[] board = { // 20x20
+			"wwwwwwwwwwwwwwwwwwww", "w        ww        w", "w w  w  www w  w  ww", "w w  w   ww w  w  ww",
+			"w  w               w", "w w w w w w w  w  ww", "w w     www w  w  ww", "w w     w w w  w  ww",
+			"w   w w  w  w  w   w", "w     w  w  w  w   w", "w ww ww        w  ww", "w  w w    w    w  ww",
+			"w        ww w  w  ww", "w         w w  w  ww", "w        w     w  ww", "w  w              ww",
+			"w  w www  w w  ww ww", "w w      ww w     ww", "w   w   ww  w      w", "wwwwwwwwwwwwwwwwwwww" };
+
 	// -------------------------------------------
-	// | Maze: (0,0)              | Score: (1,0) |
+	// | Maze: (0,0) | Score: (1,0) |
 	// |-----------------------------------------|
-	// | boardGrid (0,1)          | scorelist    |
-	// |                          | (1,1)        |
+	// | boardGrid (0,1) | scorelist |
+	// | | (1,1) |
 	// -------------------------------------------
 
 	@Override
@@ -78,19 +65,19 @@ public class Main2 extends Application {
 		try {
 			openConnection();
 			Thread t = new Thread(new Runnable() {
-		         public void run()
-		         {
-		        	 while(true) {
-		        		 try {
-		            		 while(in.hasNext()) {
-		     					String response = in.next();
-		     					handleResponse(response);
-		             		 }
+				@Override
+				public void run() {
+					while (true) {
+						try {
+							while (in.hasNext()) {
+								String response = in.next();
+								handleResponse(response);
+							}
 						} catch (Exception e) {
 						}
-		        	 }
-		         }
-		});
+					}
+				}
+			});
 			t.setDaemon(true);
 			t.start();
 			GridPane grid = new GridPane();
@@ -100,115 +87,142 @@ public class Main2 extends Application {
 
 			Text mazeLabel = new Text("Maze:");
 			mazeLabel.setFont(Font.font("Arial", FontWeight.BOLD, 20));
-	
+
 			Text scoreLabel = new Text("Score:");
 			scoreLabel.setFont(Font.font("Arial", FontWeight.BOLD, 20));
 
 			scoreList = new TextArea();
-			
+
 			GridPane boardGrid = new GridPane();
 
-			image_wall  = new Image(getClass().getResourceAsStream("Image/wall4.png"),size,size,false,false);
-			image_floor = new Image(getClass().getResourceAsStream("Image/floor1.png"),size,size,false,false);
+			image_wall = new Image(getClass().getResourceAsStream("Image/wall4.png"), size, size, false, false);
+			image_floor = new Image(getClass().getResourceAsStream("Image/floor1.png"), size, size, false, false);
 
-			hero_right  = new Image(getClass().getResourceAsStream("Image/heroRight.png"),size,size,false,false);
-			hero_left   = new Image(getClass().getResourceAsStream("Image/heroLeft.png"),size,size,false,false);
-			hero_up     = new Image(getClass().getResourceAsStream("Image/heroUp.png"),size,size,false,false);
-			hero_down   = new Image(getClass().getResourceAsStream("Image/heroDown.png"),size,size,false,false);
+			hero_right = new Image(getClass().getResourceAsStream("Image/heroRight.png"), size, size, false, false);
+			hero_left = new Image(getClass().getResourceAsStream("Image/heroLeft.png"), size, size, false, false);
+			hero_up = new Image(getClass().getResourceAsStream("Image/heroUp.png"), size, size, false, false);
+			hero_down = new Image(getClass().getResourceAsStream("Image/heroDown.png"), size, size, false, false);
 
 			fields = new Label[20][20];
-			for (int j=0; j<20; j++) {
-				for (int i=0; i<20; i++) {
+			for (int j = 0; j < 20; j++) {
+				for (int i = 0; i < 20; i++) {
 					switch (board[j].charAt(i)) {
 					case 'w':
 						fields[i][j] = new Label("", new ImageView(image_wall));
 						break;
-					case ' ':					
+					case ' ':
 						fields[i][j] = new Label("", new ImageView(image_floor));
 						break;
-					default: throw new Exception("Illegal field value: "+board[j].charAt(i) );
+					default:
+						throw new Exception("Illegal field value: " + board[j].charAt(i));
 					}
 					boardGrid.add(fields[i][j], i, j);
 				}
 			}
 			scoreList.setEditable(false);
-			
-			
-			grid.add(mazeLabel,  0, 0); 
-			grid.add(scoreLabel, 1, 0); 
-			grid.add(boardGrid,  0, 1);
-			grid.add(scoreList,  1, 1);
-						
-			Scene scene = new Scene(grid,scene_width,scene_height);
+
+			grid.add(mazeLabel, 0, 0);
+			grid.add(scoreLabel, 1, 0);
+			grid.add(boardGrid, 0, 1);
+			grid.add(scoreList, 1, 1);
+
+			Scene scene = new Scene(grid, scene_width, scene_height);
 			primaryStage.setScene(scene);
 			primaryStage.show();
 
 			scene.addEventFilter(KeyEvent.KEY_PRESSED, event -> {
 				switch (event.getCode()) {
-				case UP:    playerMoved(0,-1,"up");    break;
-				case DOWN:  playerMoved(0,+1,"down");  break;
-				case LEFT:  playerMoved(-1,0,"left");  break;
-				case RIGHT: playerMoved(+1,0,"right"); break;
-				default: break;
+				case UP:
+					playerMoved(me, 0, -1, "up");
+					break;
+				case DOWN:
+					playerMoved(me, 0, +1, "down");
+					break;
+				case LEFT:
+					playerMoved(me, -1, 0, "left");
+					break;
+				case RIGHT:
+					playerMoved(me, +1, 0, "right");
+					break;
+				default:
+					break;
 				}
 			});
-			
-            // Setting up standard players
-			
-			me = new Player("Mikkel",4,4,"up");
+
+			// Setting up standard players
+
+			me = new Player("Mikkel", 4, 4, "up");
 			addNewPlayer(me);
-			
-			sendCommand("addPlayer " + me.getName() + " " + me.getXpos() + " " + me.getYpos() + " " + me.getDirection());
-			
+
+			sendCommand(
+					"addPlayer " + me.getName() + " " + me.getXpos() + " " + me.getYpos() + " " + me.getDirection());
+
 			fields[9][4].setGraphic(new ImageView(hero_up));
-			
-			
+
 			scoreList.setText(getScoreList());
-		} catch(Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public void addNewPlayer(Player player) {
 		players.add(player);
 		fields[player.getXpos()][player.getYpos()].setGraphic(new ImageView(hero_up));
 		scoreList.setText(getScoreList());
 	}
 
-	public void playerMoved(int delta_x, int delta_y, String direction) {
-		me.direction = direction;
-		int x = me.getXpos(),y = me.getYpos();
-
-		if (board[y+delta_y].charAt(x+delta_x)=='w') {
-			me.addPoints(-1);
-		} 
-		else {
-			Player p = getPlayerAt(x+delta_x,y+delta_y);
-			if (p!=null) {
-              me.addPoints(10);
-              p.addPoints(-10);
+	public static Player getPlayer(String name) {
+		Player player = null;
+		boolean found = false;
+		int i = 0;
+		while (!found && i < players.size()) {
+			if (players.get(i).getName().equals(name)) {
+				player = players.get(i);
+				found = true;
 			} else {
-				me.addPoints(1);
-			
+				i++;
+			}
+		}
+		return player;
+	}
+
+	public void playerMoved(Player player, int delta_x, int delta_y, String direction) {
+		player.direction = direction;
+		int x = player.getXpos(), y = player.getYpos();
+
+		if (board[y + delta_y].charAt(x + delta_x) == 'w') {
+			player.addPoints(-1);
+		} else {
+			Player p = getPlayerAt(x + delta_x, y + delta_y);
+			if (p != null) {
+				player.addPoints(10);
+				p.addPoints(-10);
+			} else {
+				player.addPoints(1);
+
 				fields[x][y].setGraphic(new ImageView(image_floor));
-				x+=delta_x;
-				y+=delta_y;
+				x += delta_x;
+				y += delta_y;
 
 				if (direction.equals("right")) {
 					fields[x][y].setGraphic(new ImageView(hero_right));
-				};
+				}
+				;
 				if (direction.equals("left")) {
 					fields[x][y].setGraphic(new ImageView(hero_left));
-				};
+				}
+				;
 				if (direction.equals("up")) {
 					fields[x][y].setGraphic(new ImageView(hero_up));
-				};
+				}
+				;
 				if (direction.equals("down")) {
 					fields[x][y].setGraphic(new ImageView(hero_down));
-				};
+				}
+				;
 
-				me.setXpos(x);
-				me.setYpos(y);
+				player.setXpos(x);
+				player.setYpos(y);
 			}
 		}
 		scoreList.setText(getScoreList());
@@ -217,61 +231,67 @@ public class Main2 extends Application {
 	public String getScoreList() {
 		StringBuffer b = new StringBuffer(100);
 		for (Player p : players) {
-			b.append(p+"\r\n");
+			b.append(p + "\r\n");
 		}
 		return b.toString();
 	}
 
 	public Player getPlayerAt(int x, int y) {
 		for (Player p : players) {
-			if (p.getXpos()==x && p.getYpos()==y) {
+			if (p.getXpos() == x && p.getYpos() == y) {
 				return p;
 			}
 		}
 		return null;
 	}
-	
+
 	public void openConnection() throws IOException {
 		s = new Socket("localhost", 6789);
 		s.setKeepAlive(true);
-		
+
 		instream = s.getInputStream();
 		outstream = s.getOutputStream();
 		in = new Scanner(instream);
 		out = new PrintWriter(outstream);
 	}
-	
+
 	public void sendCommand(String command) throws IOException {
 		out.print(command + "\n");
 		out.flush();
 	}
-	
+
 	public void recieveAnswer() throws IOException {
 
 	}
-	
+
 	public void handleResponse(String response) {
-		if(response.equals("addPlayer")) {
+		if (response.equals("addPlayer")) {
 			String newPlayerName = in.next();
 			int xPos = in.nextInt();
 			int yPos = in.nextInt();
 			String direction = in.next();
-			System.out.println(newPlayerName + " " + xPos + " " +  yPos + " " + direction);
-			if(!(newPlayerName.equals(me.getName())) ) {
+			System.out.println(newPlayerName + " " + xPos + " " + yPos + " " + direction);
+			if (!(newPlayerName.equals(me.getName()))) {
 				Player newPlayer = new Player(newPlayerName, xPos, yPos, direction);
-	             Platform.runLater(new Runnable() {
-	                 @Override public void run() {
-	                	 addNewPlayer(newPlayer);
-	                 }
-	             });
+				Platform.runLater(new Runnable() {
+					@Override
+					public void run() {
+						addNewPlayer(newPlayer);
+					}
+				});
 			}
+		} else if (response.equals("PlayerMoves")) {
+			String playerName = in.next();
+			Player player = getPlayer(playerName);
+			int xPos = in.nextInt();
+			int yPos = in.nextInt();
+			String direction = in.next();
+			playerMoved(player, xPos, yPos, direction);
 		}
 	}
-	
 
 	public static void main(String[] args) {
 		launch(args);
 	}
 
 }
-
